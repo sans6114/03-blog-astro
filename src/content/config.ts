f71e89e59d7da51dd4fe2bf4ee3b120174e262a1
Santiago Sosa <santisosalp@gmail.com>
2024-07-13 16:00:58 -0300

    problemas con componeente image

---
+++ src/content/config.ts
@@ -0,0 +1,23 @@
+import {
+  defineCollection,
+  z,
+} from 'astro:content';
+
+const blogCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string(),
+        date: z.date(),
+        description: z.string(),
+        image: z.string(),
+        //relacion
+        author: z.string(),
+        //relacion
+        tags: z.array(z.string()),
+    })
+})
+
+
+export const collections = {
+    blog: blogCollection
+}
