b6295c33d7e374e94f3673e88b29c988f8d3ebb6
Santiago Sosa <santisosalp@gmail.com>
2024-07-13 12:34:57 -0300

    mi primer commit

---
+++ src/congif/site-config.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+title: 'The Santi blog',
+description: 'This is a simple blog to improve in collection skils'
+
+}
---
+++ src/interfaces/post-interface.ts
@@ -0,0 +1,8 @@
+export interface PostProps {
+    title: string;
+    date: string;
+    description: string;
+    author: string;
+    image: string;
+    tags: string[];
+  }
