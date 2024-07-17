654eefa917174f1a0e8b4c4f56e42abdf4105b10
Santiago Sosa <santisosalp@gmail.com>
2024-07-17 17:25:35 -0300

    problemas con referencias

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,6 @@
 import {
   defineCollection,
+  reference,
   z,
 } from 'astro:content';
 
@@ -9,17 +10,38 @@ const blogCollection = defineCollection({
         title: z.string(),
         date: z.date(),
         description: z.string(),
-        author: z.string(),
-        //relacion
         image: image().refine(img => img.width > 500, {
             message: 'image should be lower than 500px'
         }),
         //relacion
-        tags: z.array(z.string()),
+        //author: z.string(),
+        author: reference('author'),
+        //relacion
+        tags: reference('tags'),
+    })
+})
+
+
+const authorCollection = defineCollection({
+    type: 'data',
+    schema: ({image}) => z.object({
+        name: z.string(),
+        avatar: image().refine((img) => img.width > 300, {
+            message: 'you image should be lower than 300px'
+        })
+    })
+})
+
+const tagsCollection = defineCollection({
+    type: 'data',
+    schema: () => z.object({
+    tags:  z.array(z.string())
     })
 })
 
 
 export const collections = {
-    blog: blogCollection
+    blog: blogCollection,
+    author: authorCollection,
+    tags: tagsCollection
 }
