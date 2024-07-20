4c376302e49230a5077accff981142eecd966fa2
Santiago Sosa <santisosalp@gmail.com>
2024-07-20 19:52:48 -0300

    cambios en rss.xml

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,38 +1,60 @@
 import type { APIRoute } from 'astro';
 import { getCollection } from 'astro:content';
+import markdownIt from 'markdown-it';
+import sanitizeHtml from 'sanitize-html';
 
 import rss from '@astrojs/rss';
 
-export const GET: APIRoute = async({ params, request, site }) => {
+const parser = new markdownIt()
 
 
-
-  const blogPost = await getCollection('blog')
-console.log(blogPost)
+export const GET: APIRoute = async ({ params, request, site }) => {
+  const blogPost = await getCollection("blog");
+  console.log(blogPost);
 
   return rss({
-     stylesheet: '/styles/rss.xsl',
+    stylesheet: "/styles/rss.xsl",
     // `<title>` field in output xml
-    title: 'Santi Blog',
+    title: "Santi Blog",
     // `<description>` field in output xml
-    description: 'Un blog simple con el proposito de aprender cosas nuevas en Astro framework',
+    description:
+      "Un blog simple con el proposito de aprender cosas nuevas en Astro framework",
+
+
+      xmlns: {
+        media: 'http://search.yahoo.com/mrss/',
+      },
     // Pull in your project "site" from the endpoint context
     // https://docs.astro.build/en/reference/api-reference/#contextsite
-    site: site ?? '',
+    site: site ?? "",
     // Array of `<item>`s in output xml
     // See "Generating items" section for examples using content collections and glob imports
-    items: blogPost.map(post => ({
-title: post.data.title,
-pubDate: post.data.date,
-description: post.data.description,
-link: `posts/${post.slug}`
+    items: blogPost.map(({data, slug, body}) => ({
+      title: data.title,
+      pubDate: data.date,
+      description: data.description,
+      link: `posts/${slug}`,
+
+
+
+      content: sanitizeHtml(parser.render(body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+      }),
+      
+      customData: `<media:content
+          type="image/${data.image.format === 'jpg' ? 'jpeg' : 'png'}"
+          width="${data.image.width}"
+          height="${data.image.height}"
+          medium="image"
+          url="${site + data.image.src}" />
+      `,
+      
+      xmlns: {
+        media: 'http://search.yahoo.com/mrss/',
+      },
+      
     })),
     // (optional) inject custom xml
     customData: `<language>es</language>`,
   });
-
-
-  }
-
-
-
+};
