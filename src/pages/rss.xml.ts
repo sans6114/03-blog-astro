d0b8503988ccf492f262437e0545088668556118
Santiago Sosa <santisosalp@gmail.com>
2024-07-20 17:59:50 -0300

    blog casi terminado

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,9 +1,38 @@
 import type { APIRoute } from 'astro';
+import { getCollection } from 'astro:content';
 
-export const GET: APIRoute = ({ params, request }) => {
-    return new Response(JSON.stringify({
-        hola: 'mundo',
-        name: 'santi'
-      })
-    )
+import rss from '@astrojs/rss';
+
+export const GET: APIRoute = async({ params, request, site }) => {
+
+
+
+  const blogPost = await getCollection('blog')
+console.log(blogPost)
+
+  return rss({
+     stylesheet: '/styles/rss.xsl',
+    // `<title>` field in output xml
+    title: 'Santi Blog',
+    // `<description>` field in output xml
+    description: 'Un blog simple con el proposito de aprender cosas nuevas en Astro framework',
+    // Pull in your project "site" from the endpoint context
+    // https://docs.astro.build/en/reference/api-reference/#contextsite
+    site: site ?? '',
+    // Array of `<item>`s in output xml
+    // See "Generating items" section for examples using content collections and glob imports
+    items: blogPost.map(post => ({
+title: post.data.title,
+pubDate: post.data.date,
+description: post.data.description,
+link: `posts/${post.slug}`
+    })),
+    // (optional) inject custom xml
+    customData: `<language>es</language>`,
+  });
+
+
   }
+
+
+
